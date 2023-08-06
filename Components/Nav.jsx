7615e608a88c62041e1bed462b0c73262b32b060
Ayush <unitedestates.ayush10@gmail.com>
2023-08-07 03:08:08 +0530

    nav buttons and signup functionality for button

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -7,6 +7,15 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+    };
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -20,6 +29,32 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
       {/* ======Mobile Navogation=========*/}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-promt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/user.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+                // onClick={}
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
