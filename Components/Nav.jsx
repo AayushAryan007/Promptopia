475f4b2bf9ece319901fe7dbf8338511a98124cf
Ayush <unitedestates.ayush10@gmail.com>
2023-08-07 22:01:46 +0530

    session user auth

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -7,7 +7,8 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  // const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -31,9 +32,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+      {alert(session?.user)}
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-promt" className="black_btn">
               Create Post
@@ -73,7 +75,7 @@ const Nav = () => {
 
       {/* ======Mobile Navigation=========*/}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/user.svg"
